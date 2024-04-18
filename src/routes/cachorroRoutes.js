dca871e339c53ade0cd6a881b3021d08dccf22ce
FelipeOropeza <felipe2006.co@gmail.com>
2024-04-18 11:50:55 -0300

    chore: rotas pra model cachorro

--- src/routes/cachorroRoutes.js
+++ src/routes/cachorroRoutes.js
@@ -4,3 +4,6 @@ import CachorroController from "../controllers/cachorroController.js";
 const routes = express.Router();
 
 routes.get("/cachorros", CachorroController.listaCachorros);
+routes.post("/cachorros", CachorroController.cadastrarCachorro);
+
+export default routes;
