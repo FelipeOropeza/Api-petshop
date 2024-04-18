6b70415570d6853f5538c62632c54c035186e9dc
FelipeOropeza <felipe2006.co@gmail.com>
2024-04-18 11:50:37 -0300

    chore: cadastra cachorro

--- src/controllers/cachorroController.js
+++ src/controllers/cachorroController.js
@@ -5,6 +5,20 @@ class CachorroController {
     const listaCachorros = await cachorro.find({});
     res.status(200).json(listaCachorros);
   }
+
+  static async cadastrarCachorro(req, res) {
+    try {
+      const novoCachorro = await cachorro.create(req.body);
+      res.status(201).json({
+        message: "Cachorro inserido com sucesso.",
+        cachorro: novoCachorro,
+      });
+    } catch (erro) {
+      res.status(500).json({
+        message: `${erro.message} - Falha ao casdastrar cachorro`,
+      });
+    }
+  }
 }
 
 export default CachorroController;
