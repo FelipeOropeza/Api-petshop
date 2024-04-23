360e28bbbc7c39fe80f11fd4c6a8ab08fba6446c
FelipeOropeza <felipe2006.co@gmail.com>
2024-04-23 11:28:09 -0300

    chore: novas rotas pra o crud

--- src/routes/cachorroRoutes.js
+++ src/routes/cachorroRoutes.js
@@ -4,6 +4,9 @@ import CachorroController from "../controllers/cachorroController.js";
 const routes = express.Router();
 
 routes.get("/cachorros", CachorroController.listaCachorros);
+routes.get("/cachorros/:id", CachorroController.buscarCachorroPorId);
 routes.post("/cachorros", CachorroController.cadastrarCachorro);
+routes.put("/cachorros/:id", CachorroController.atualizarCachorro);
+routes.delete("/cachorros/:id", CachorroController.deletarCachorro);
 
 export default routes;
